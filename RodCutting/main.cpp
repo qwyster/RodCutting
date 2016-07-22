441224f66d5c0774263f443b8bf32e7ead9d269d
nam_nguyen <qwyster@gmail.com>
2016-07-22 10:12:51 +0200

    implementation by myself

--- RodCutting/main.cpp
+++ RodCutting/main.cpp
@@ -6,11 +6,35 @@
 #include "RodCutting.h"
 #include<ctime>
 
+/*
+	Solve the rod cutting problem for a rod of length l.
+	Return the highest price.
+*/
+int rodCutting(int* p, int length, int l, int* cutSeq) {
+	int result;
+	int* optimal = new int[length];
+	for (int k = 1; k <= l; k++) {
+		// find optimal[k]
+		optimal[k-1] = p[k-1];
+		cutSeq[k - 1] = k;
+		for (int i = 1; i <= k - 1; i++) {
+			int newPrice = p[i - 1] + optimal[k - i - 1];
+			if (newPrice > optimal[k-1]) {
+				cutSeq[k - 1] = i;
+				optimal[k-1] = newPrice;
+			}
+		}
+	}
+	result = optimal[l-1];
+	delete[] optimal;
+	return result;
+}
+
 int main()
 {
 	
 
-	RodCutting rc;
+	/*RodCutting rc;
 	int length = 8;
 	
 	clock_t begin1 = clock();
@@ -34,7 +58,22 @@ int main()
 	}
 	delete[] s;
 	std::cout << std::endl;
-	std::cin.get();
-    return 0;
+	std::cin.get();*/
+	const int length = 10;
+	int* p = new int[length] {1,5,8,9,10,17,17,20,24,30};
+	int* cutSeq = new int[length];
+	cutSeq[0] = 1;
+	int l = 8;
+	std::cout << "Optimal value of cutting a rod of length " << l << " is " << rodCutting(p,length,l,cutSeq) << std::endl;
+	std::cout << "Solution to this problem: ";
+	int r = l;
+	while (r > 0) {
+		std::cout << cutSeq[r-1] << " ";
+		r -= cutSeq[r - 1];
+	}
+	std::cout << std::endl;
+	delete[] cutSeq;
+	delete[] p;
+	return 0;
 }
 
